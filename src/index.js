04a8543b2eb585fdfeea262ec51d4ebc6d88fa83
Dimitris Theodosopoulos <155730305+dimtheo6@users.noreply.github.com>
2024-08-14 17:58:46 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import loadOrder from './order'
 
 loadHome();
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+  }
+
 document.getElementById('homeBtn').addEventListener('click',()=>{
     document.getElementById('content').innerHTML = "";
     loadHome();
